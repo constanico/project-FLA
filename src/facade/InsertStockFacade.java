5321ad9485b503bf50bb78c17681cbf0a4e6597d
daplust <106513004+daplust@users.noreply.github.com>
2023-01-16 05:57:36 +0700

    add InsertStockFacade

---
+++ src/facade/InsertStockFacade.java
@@ -0,0 +1,26 @@
+package facade;
+
+import model.Coffee;
+import model.Milk;
+import model.Water;
+import singleton.Database;
+
+public class InsertStockFacade {
+    
+    Database db = Database.getDatabase();
+    Coffee coffee;
+    Milk milk;
+    Water water;
+    private Integer waterStock, milkStock, coffeeStock;
+    public InsertStockFacade(Integer waterStock, Integer milkStock, Integer coffeeStock){
+        this.waterStock = waterStock;
+        this.milkStock = milkStock;
+        this.coffeeStock = coffeeStock;
+    }
+
+    public void insertingStock(){
+        db.getStock().put(coffee, coffeeStock);
+        db.getStock().put(water, waterStock);
+        db.getStock().put(milk, milkStock);
+    }
+}
